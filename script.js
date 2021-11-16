81578be061b7b865cca317aa54f59842ae6d5b7d
ElenaLj <elenaljubijankic@gmail.com>
2021-11-16 16:42:15 +0100

    esercizio pari e dispari

--- script.js
+++ script.js
@@ -7,4 +7,52 @@
 // 3) Fai una funzione per generare un numero random per il computer;
 // 4) Fai la somma del numero-utente con numero-computer;
 // 5) Per stabilire il vincitore ti serve un'altra funzione per controllare se la somma è pari o dispari;
-// 6) Dichiara il vincitore
+// 6) Dichiara il vincitore;
+
+// 1)
+const evenOdd = prompt("Scegli pari o dispari"); 
+//il prompt conserverà già il valore "pari" o "dispari" che poi andrò a confrontare;
+console.log(evenOdd);
+
+// 2)
+const insertNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
+//anche qui il prompt conserva il valore che mi servirà per dopo;
+console.log(insertNumber);
+
+// 3) 
+function computerNumber(min, max) {
+    const randomNumber = Math.floor(Math.random() * (max - min + 1) ) + min;
+    return randomNumber;
+    // qui non ha senso fare il console.log perché la funzione non è ancora stata invocata e il blocco di codice per ora è inattivo;
+}
+
+// 3.1) Mi serve invocare la funzione con una variabile, così da poterla usare;
+const randomComputer = computerNumber(1, 5);
+console.log(randomComputer);
+
+// 4) 
+const sum = insertNumber + randomComputer;
+console.log(sum);
+
+// 5)
+function numberType (numero) {
+    if (numero % 2 == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+// devo invocare di nuovo la funzione per poterla usare
+const result = numberType(sum);
+console.log(result);
+
+// 6)
+if (evenOdd == "pari" && result == true) {
+    console.log("Hai vinto");
+} else if (evenOdd == "dispari" && result == false) {
+    console.log("Hai vinto");
+} else {
+    console.log("Hai perso :( ");
+}
