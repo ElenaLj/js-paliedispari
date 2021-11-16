4c71be037118f879c6e3c780455e9e2a1cecd7e2
ElenaLj <elenaljubijankic@gmail.com>
2021-11-16 15:50:17 +0100

    init esercizio

---
+++ script.js
@@ -0,0 +1,10 @@
+// alert("Hello World!");
+
+// Pari e Dispari L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
+
+// 1) Chiedere all'utente pari o dispari - conservane il valore;
+// 2) Chiedere all'utente un numero da 1 a 5 - conservane il valore;
+// 3) Fai una funzione per generare un numero random per il computer;
+// 4) Fai la somma del numero-utente con numero-computer;
+// 5) Per stabilire il vincitore ti serve un'altra funzione per controllare se la somma è pari o dispari;
+// 6) Dichiara il vincitore
